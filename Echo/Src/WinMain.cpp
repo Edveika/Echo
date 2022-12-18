b530a8d0991c2056249eb0bc02fd8b48ab093bed
Edveika <113787144+Edveika@users.noreply.github.com>
2022-12-18 23:33:15 +0200

    Add files via upload

--- Echo/Src/WinMain.cpp
+++ Echo/Src/WinMain.cpp
@@ -7,8 +7,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	float curGameFrame = 0;
 	Engine* engine = new Engine(hInstance);
-	Sprite* sprite = new Sprite(engine->gfx, L"Jump.png", { 96,96 }, 8, 2);
-	Sprite* sprite1 = new Sprite(engine->gfx, L"Run.png", { 96,96 }, 7, 2);
+	Sprite* sprite = new Sprite(engine->gfx, L"Jump.png", { 96,96 }, 2);
+	Sprite* sprite1 = new Sprite(engine->gfx, L"Run.png", { 96,96 }, 2);
 	Timer* timer = new Timer();
 	
 	MSG msg;
@@ -30,9 +30,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			engine->dx9->pd3dDevice->BeginScene();
 
 			sprite->Draw({ 50,50 }, 0XFFFFFFFF, timer->GetTime(0.1f));
-			///sprite1->Draw({ 250,50 }, 0XFFFFFFFF, timer->GetAnimRate(), 10);
-			//sprite1->Draw({ 250,50 }, 0XFFFFFFFF, curGameFrame, 2);
-
+			sprite1->Draw({ 50,50 }, 0XFFFFFFFF, timer->GetTime(0.1f));
+			
 			engine->dx9->pd3dDevice->EndScene();
 
 			// Present the back buffer contents to the display
